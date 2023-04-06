43eaeaac6bc9fb7f655a4d29d1aa7d505cb0443b
Ehfaz Mahee <mahee504@gmail.com>
2023-04-07 04:43:51 +0600

    2nd

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import Tshirt from "../Tshirt/Tshirt";
+import Cart from "../Cart/Cart";
+import './Home.css'
 const Home = () => {
-    const tshirts = useLoaderData();
+  const tshirts = useLoaderData();
+  const [cart,setCart]= useState([]);
+  
+    const handleAddToCart = tshirt =>{
+        console.log(tshirt);
+    }
 
-    return (
-        <div>
-            <h2>This is home{tshirts.length}</h2>
-        </div>
-    );
+
+  return (
+    <div className="home-container">
+      <div className="tshirts-container">
+        {tshirts.map((tshirt) => (
+          <Tshirt key={tshirt._id} tshirt={tshirt}
+          handleAddToCart={handleAddToCart}
+          ></Tshirt>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart></Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/Tshirt/Tshirt.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Tshirt.css'
+const Tshirt = ({tshirt,handleAddToCart}) => {
+    const {picture,name,price}=tshirt;
+    return (
+        <div className='tshirt'>
+            <img src={picture} alt="" />
+            <h4>Name Of Shirt: {name}</h4>
+            <h4>Price Of Shirt: {price}</h4>
+            <button onClick={()=>{handleAddToCart}}>Buy Me Now</button>
+        </div>
+    );
+};
+
+export default Tshirt;
